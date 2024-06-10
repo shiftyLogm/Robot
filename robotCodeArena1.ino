f983bb02e40f7215d8c5efcade2c9ba19458e077
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-06-10 14:04:50 -0300

    comments

--- robotCodeArena1.ino
+++ robotCodeArena1.ino
@@ -1,44 +1,51 @@
 #include <Servo.h>
+#include <AFMotor.h>
 
-const int pinServo1 = 1;
-const int pinServo2 = 2;
-Servo gripperServo1;
-Servo gripperServo2;
+#define PINSERVO1 1;
+#define PINSERVO2 2;
 #define PINSENSOR 10
 #define PINLED 11
 #define ENERGYPIN 13
+
+Servo gripperServo1;
+Servo gripperServo2;
 int ReadSensor;
+
+AF_DCMotor motor(1); // Definindo pino de entrada para o motor, no caso M1
+AF_DCMotor motor2(4);
 
 void setup(){
 
-  pinMode(PINSENSOR, INPUT);
-  pinMode(PINLED, OUTPUT);
-  pinMode(ENERGYPIN, OUTPUT);
-  gripperServo1.attach(pinServo1);
-  gripperServo2.attach(pinServo2);
+  pinMode(PINSENSOR, INPUT); // Definindo pino do sensor como input
+  pinMode(ENERGYPIN, OUTPUT); // Definindo a entrda do pino de energia
+  
+  gripperServo1.attach(PINSERVO1); // Atribuindo energia ao pino do servo
+  gripperServo2.attach(PINSERVO2);
 
+  motor.setSpeed(200); // Definindo velocidade do Motor
+  motor2.setSpeed(200); 
+  motor.run(FORWARD); // Definindo a direção para frente (Isso também liga o motor)
+  motor2.run(FORWARD);
+  
 }
 
 void loop(){]
 
-  digitalWrite(ENERGYPIN, HIGH);
-  ReadSensor = digitalRead(PINSENSOR);
+  digitalWrite(ENERGYPIN, HIGH); // Atribuindo energia ao pino
+  ReadSensor = digitalRead(PINSENSOR); // Variável que armazena o atual valor do sensor IF em loop
 
   if(ReadSensor == LOW){
-    digitalWrite(PINLED, HIGH);
-    Serial.println(PINSENSOR);
+    // Condição para caso o sensor detecte "LOW"
   }
 
   else{
-    digitalWrite(PINLED, LOW);
+    // Método para caso seja falso
   }
 
-  for(int pos = 0; pos <= 180; pos++){
-    gripperServo1.write(pos);
-    delay(15);
-  }
-  for (int pos = 180; pos >= 0; pos -= 1) {
-    gripperServo1.write(pos);
-    delay(15);
-  }
+  motor.setSpeed(200); // Definindo velocidade do Motor
+  motor2.setSpeed(200); 
+  motor.run(FORWARD); // Definindo a direção para a frente
+  motor2.run(FORWARD);
+  motor.run(BACKWARD); // Definindo a direção para trás
+  motor2.run(BACKWARD); // motor.run(RELEASE) => Desliga o motor    
 }
