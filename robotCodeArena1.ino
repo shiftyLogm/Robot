2a64a1384a16d925b1f37237c5c7b831957f9011
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-06-10 10:37:19 -0300

    Update robotCodeArena1.ino

--- robotCodeArena1.ino
+++ robotCodeArena1.ino
@@ -4,15 +4,34 @@ const int pinServo1 = 1;
 const int pinServo2 = 2;
 Servo gripperServo1;
 Servo gripperServo2;
+#define PINSENSOR 10
+#define PINLED 11
+#define ENERGYPIN 13
+int ReadSensor;
 
 void setup(){
 
+  pinMode(PINSENSOR, INPUT);
+  pinMode(PINLED, OUTPUT);
+  pinMode(ENERGYPIN, OUTPUT);
   gripperServo1.attach(pinServo1);
   gripperServo2.attach(pinServo2);
 
 }
 
-void loop(){
+void loop(){]
+
+  digitalWrite(ENERGYPIN, HIGH);
+  ReadSensor = digitalRead(PINSENSOR);
+
+  if(ReadSensor == LOW){
+    digitalWrite(PINLED, HIGH);
+    Serial.println(PINSENSOR);
+  }
+
+  else{
+    digitalWrite(PINLED, LOW);
+  }
 
   for(int pos = 0; pos <= 180; pos++){
     gripperServo1.write(pos);
