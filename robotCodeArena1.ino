3ed2c3e3e716cce80141e5976b0f185dc64cdc12
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-06-11 14:15:31 -0300

    Update robotCodeArena1.ino

--- robotCodeArena1.ino
+++ robotCodeArena1.ino
@@ -1,51 +1,60 @@
 #include <Servo.h>
 #include <AFMotor.h>
 
-#define PINSERVO1 1;
-#define PINSERVO2 2;
-#define PINSENSOR 10
-#define PINLED 11
-#define ENERGYPIN 13
+#define PINSERVO1 
+#define PINSERVO2 
+#define PIN_IFSENSOR1 
+#define PIN_IFSENSOR2 
+#define PINLED 
+#define ENERGYPIN 
 
 Servo gripperServo1;
 Servo gripperServo2;
-int ReadSensor;
+int Read_IFSensor1;
+int Read_IFSensor2;
 
-AF_DCMotor motor(1); // Definindo pino de entrada para o motor, no caso M1
-AF_DCMotor motor2(4);
+AF_DCMotor Dc_Motor1(1); // Definindo pino de entrada para o motor, no caso M1
+AF_DCMotor Dc_Motor2(4);
 
 void setup(){
 
-  pinMode(PINSENSOR, INPUT); // Definindo pino do sensor como input
+  pinMode(PIN_IFSENSOR1, INPUT); // Definindo pino do sensor como input
+  pinMode(PIN_IFSENSOR2, INPUT);
   pinMode(ENERGYPIN, OUTPUT); // Definindo a entrda do pino de energia
   
-  gripperServo1.attach(PINSERVO1); // Atribuindo energia ao pino do servo
-  gripperServo2.attach(PINSERVO2);
+  gripperServo1.attach(PIN_IFSERVO1); // Atribuindo energia ao pino do servo
+  gripperServo2.attach(PIN_IFSERVO2);
 
-  motor.setSpeed(200); // Definindo velocidade do Motor
-  motor2.setSpeed(200); 
-  motor.run(FORWARD); // Definindo a direção para frente (Isso também liga o motor)
-  motor2.run(FORWARD);
+  Dc_Motor1.setSpeed(200); // Definindo velocidade do Motor
+  Dc_Motor2.setSpeed(200); 
+  Dc_Motor1.run(FORWARD); // Definindo a direção para frente (Isso também liga o motor)
+  Dc_Motor2.run(FORWARD);
   
 }
 
-void loop(){]
+void loop(){
 
   digitalWrite(ENERGYPIN, HIGH); // Atribuindo energia ao pino
-  ReadSensor = digitalRead(PINSENSOR); // Variável que armazena o atual valor do sensor IF em loop
+  Read_IFSensor1 = digitalRead(PIN_IFSENSOR1); // Variável que armazena o atual valor do sensor IF em loop
+  Read_IFSensor2 = digitalRead(PIN_IFSENSOR2);
 
-  if(ReadSensor == LOW){
-    // Condição para caso o sensor detecte "LOW"
+  if(Read_IFSensor1 == HIGH && Read_IFSensor2 == HIGH){
+    delay(500);
+    Dc_Motor1.run(BACKWARD);
+    Dc_Motor2.run(BACKWARD);
   }
 
   else{
     // Método para caso seja falso
   }
 
-  motor.setSpeed(200); // Definindo velocidade do Motor
-  motor2.setSpeed(200); 
-  motor.run(FORWARD); // Definindo a direção para a frente
-  motor2.run(FORWARD);
-  motor.run(BACKWARD); // Definindo a direção para trás
-  motor2.run(BACKWARD); // motor.run(RELEASE) => Desliga o motor    
+  Dc_Motor1.setSpeed(200); // Definindo velocidade do Motor
+  Dc_Motor2.setSpeed(200); 
+  Dc_Motor1.run(FORWARD); // Definindo a direção para a frente
+  Dc_Motor2.run(FORWARD);
+  Dc_Motor1.run(BACKWARD); // Definindo a direção para trás
+  Dc_Motor2.run(BACKWARD); // motor.run(RELEASE) => Desliga o motor    
+
+  // Possível lógica
+
 }
