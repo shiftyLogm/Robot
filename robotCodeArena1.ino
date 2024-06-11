ed6eec3a6844d9c97b11c37ab08259dbdaa09806
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-06-11 15:15:21 -0300

    Update robotCodeArena1.ino

--- robotCodeArena1.ino
+++ robotCodeArena1.ino
@@ -1,26 +1,32 @@
-#include <Servo.h>
-#include <AFMotor.h>
+#include <Servo.h> // Biblioteca para manuseio do Motor Servo
+#include <AFMotor.h> // Biblioteca para manuseio do Shield Motor
+#include <Ultrasonic.h> // Biblioteca para manuseio do Sensor Ultrassônico
 
-#define PINSERVO1 
-#define PINSERVO2 
-#define PIN_IFSENSOR1 
-#define PIN_IFSENSOR2 
-#define PINLED 
+// Definições dos pinos que serão usados
+#define PINSERVO1
+#define PINSERVO2
+#define PIN_IFSENSOR1
+#define PIN_IFSENSOR2
+#define PINLED
 #define ENERGYPIN 
+#define TRIGGER
+#define ECHO
 
 Servo gripperServo1;
 Servo gripperServo2;
 int Read_IFSensor1;
 int Read_IFSensor2;
+int UltSensor_Distance; // Variável que receberá a atual distância em centímetros do objeto detectado pelo Sensor Utrassônico
 
 AF_DCMotor Dc_Motor1(1); // Definindo pino de entrada para o motor, no caso M1
 AF_DCMotor Dc_Motor2(4);
+Ultrasonic SensorUltra(TRIGGER, ECHO); // Definindo os pinos de entrada de Trigger e Echo do Ultrassônico
 
 void setup(){
 
   pinMode(PIN_IFSENSOR1, INPUT); // Definindo pino do sensor como input
   pinMode(PIN_IFSENSOR2, INPUT);
-  pinMode(ENERGYPIN, OUTPUT); // Definindo a entrda do pino de energia
+  pinMode(ENERGYPIN, OUTPUT); // Definindo a entrada do pino de energia
   
   gripperServo1.attach(PIN_IFSERVO1); // Atribuindo energia ao pino do servo
   gripperServo2.attach(PIN_IFSERVO2);
@@ -34,6 +40,7 @@ void setup(){
 
 void loop(){
 
+  UltSensor_Distance = SensorUltra.read(CM); // Passará a distância em centímetros do objecto detectado pelo Sensor
   digitalWrite(ENERGYPIN, HIGH); // Atribuindo energia ao pino
   Read_IFSensor1 = digitalRead(PIN_IFSENSOR1); // Variável que armazena o atual valor do sensor IF em loop
   Read_IFSensor2 = digitalRead(PIN_IFSENSOR2);
