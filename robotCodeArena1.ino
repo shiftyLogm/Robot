20522d4def67670308ff0808761d04bd35a5922c
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-06-13 08:18:46 -0300

    Update robotCodeArena1.ino

--- robotCodeArena1.ino
+++ robotCodeArena1.ino
@@ -8,7 +8,7 @@
 #define PIN_IFSENSOR1
 #define PIN_IFSENSOR2
 #define PIN_LED
-#define ENERGY_PIN 
+#define ENERGY_PIN 7
 #define TRIGGER
 #define ECHO
 #define IN1 A1
@@ -20,51 +20,54 @@
 #define PIN_IFSENSOR1 12
 #define PIN_IFSENSOR2 13
 
+// Variáveis Servos
 Servo gripper_Servo1;
 Servo gripper_Servo2;
-int Read_IFSensor1;
-int Read_IFSensor2;
-int UltSensor_Distance; // Variável que receberá a atual distância em centímetros do objeto detectado pelo Sensor Utrassônico
 
+// Variável que receberá a atual distância em centímetros do objeto detectado pelo Sensor Utrassônico
+int UltSensor_Distance; 
+
+// Variáveis recebendo os Motores DC na Ponte H
 L298N motor1(ENA, IN1, IN2);
 L298N motor2(ENB, IN3, IN4);
-Ultrasonic SensorUltra(TRIGGER, ECHO); // Definindo os pinos de entrada de Trigger e Echo do Ultrassônico
 
+// Definindo os pinos de entrada de Trigger e Echo do Ultrassônico
+Ultrasonic SensorUltra(TRIGGER, ECHO); 
+
+// Valores booleanos para armazenar o valor dos Sensores Infravermelhos
 bool ReadSensor1;
 bool ReadSensor2;
 
 void setup(){
 
-  pinMode(PIN_IFSENSOR1, INPUT); // Definindo pino do sensor como input
+  // Definindo pinos dos sensores como input
+  pinMode(PIN_IFSENSOR1, INPUT);
   pinMode(PIN_IFSENSOR2, INPUT);
-  pinMode(ENERGY_PIN, OUTPUT); // Definindo a entrada do pino de energia
-  pinMode(7, OUTPUT);
   
-  gripper_Servo1.attach(PIN_IFSERVO1); // Atribuindo energia ao pino do servo
+  // Definindo a entrada do pino de energia
+  pinMode(ENERGYPIN, OUTPUT);
+
+  // Atribuindo energia aos pinos dos servos
+  gripper_Servo1.attach(PIN_IFSERVO1); 
   gripper_Servo2.attach(PIN_IFSERVO2);
 
-  // Definindo velocidade do Motor
+  // Definindo velocidade dos Motores
   motor1.setSpeed(150);
   motor2.setSpeed(150);
-
-  
-  
-  Dc_Motor1.run(FORWARD); // Definindo a direção para frente (Isso também liga o motor)
-  Dc_Motor2.run(FORWARD);
   
 }
 
 void loop(){
 
   UltSensor_Distance = SensorUltra.read(CM); // Passará a distância em centímetros do objecto detectado pelo Sensor
+  
   digitalWrite(ENERGY_PIN, HIGH); // Atribuindo energia ao pino
-  Read_IFSensor1 = digitalRead(PIN_IFSENSOR1); // Variável que armazena o atual valor do sensor IF em loop
-  Read_IFSensor2 = digitalRead(PIN_IFSENSOR2);
 
-  digitalWrite(7, HIGH);
+  // Ternário Condicional que armazena os atuais valores dos sensores IF em loop, caso HIGH armazena true, caso contrário armazena falso
   ReadSensor1 = digitalRead(PIN_IFSENSOR1) == HIGH ? true : false;
   ReadSensor2 = digitalRead(PIN_IFSENSOR2) == HIGH ? true : false;
-  
+
+  // Ternário Condicional que executa uma direção para os motores, caso o valor booelano seja true ele anda, caso contrário ele para
   ReadSensor1 ? motor1.forward() : motor1.stop();
   ReadSensor2 ? motor2.forward() : motor2.stop(); 
 
