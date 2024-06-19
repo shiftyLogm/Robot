290ac9bde7a518ef303760e9378de948936a4335
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-06-18 23:57:56 -0300

    Update Arena1.ino

--- codes/Arena1.ino
+++ codes/Arena1.ino
@@ -11,43 +11,43 @@ bool ReadSensor2;
 class DCMotor {
   public:
     DCMotor(uint8_t pinA, uint8_t pinB) {
-        pin1 = pinA;
-        pin2 = pinB;
-        speed = 0;
+        _pin1 = pinA;
+        _pin2 = pinB;
+        _speed = 0;
     }
 
     void begin() {
-        pinMode(pin1, OUTPUT);
-        pinMode(pin2, OUTPUT);
+        pinMode(_pin1, OUTPUT);
+        pinMode(_pin2, OUTPUT);
     }
 
     void setSpeed(int spd) {
-        speed = spd;
+        _speed = spd;
     }
 
     void Forward() {
-        analogWrite(pin1, speed);
-        digitalWrite(pin2, LOW);
+        analogWrite(_pin1, speed);
+        digitalWrite(_pin2, LOW);
     }
 
     void Backward() {
-        analogWrite(pin2, speed);
-        digitalWrite(pin1, LOW);
+        analogWrite(_pin2, speed);
+        digitalWrite(_pin1, LOW);
     }
 
     void Stop() {
-        digitalWrite(pin1, LOW);
-        digitalWrite(pin2, LOW);
+        digitalWrite(_pin1, LOW);
+        digitalWrite(_pin2, LOW);
     }
 
     int getSpeed() {
-        return speed;
+        return _speed;
     }
 
   private:
-    uint8_t pin1;
-    uint8_t pin2;
-    int speed;
+    uint8_t _pin1;
+    uint8_t _pin2;
+    int _speed;
 };
 
 DCMotor motor1(PIN_MOTOR1A, PIN_MOTOR1B);
